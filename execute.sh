ab062c04755fc61aa96c898e1c895528c4342e08
jojoldu <jojoldu@gmail.com>
2018-04-14 12:24:11 +0900

    프로세스 자동 종료 추가

--- execute.sh
+++ execute.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+CURRENT_PID=$(pgrep -f linux-thread)
+kill -9 $CURRENT_PID
+
 /home/ec2-user/linux-thread/gradlew clean build
 
 nohup java -jar /home/ec2-user/linux-thread/build/libs/linux-thread-0.0.1-SNAPSHOT.jar &
