3b1a33c230f2eda060b965434a706d0f041f48fc
jojoldu <jojoldu@gmail.com>
2018-04-15 18:24:37 +0900

    쉘은 어려웡

--- execute.sh
+++ execute.sh
@@ -7,6 +7,4 @@ kill -9 $CURRENT_PID
 
 nohup java -jar /home/ec2-user/linux-thread/build/libs/linux-thread-0.0.1-SNAPSHOT.jar &
 
-PID=$(pgrep -f linux-thread)
-
-echo 'Current PID: $PID'
+echo $(pgrep -f linux-thread)
